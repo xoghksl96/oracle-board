7241a55a82152bbfab1dcebea5f8c4064940038b
GDJ 58 <GDJ 58@DESKTOP-RVLGL7L>
2022-12-28 10:12:42 +0900

    2022-12-28 10:12 MemberDao 수정

--- src/main/java/dao/MemberDao.java
+++ src/main/java/dao/MemberDao.java
@@ -47,6 +47,35 @@ public class MemberDao {
 	}
 	
 	// 3) 회원 정보 수정 dao
+	public int updateMemberDao(Connection conn, Member member) throws Exception {
+		int resultLow = 0;
+		
+		String sql = "UPDATE member SET"
+				+ " member_name = ?, updatedate = sysdate"
+				+ " WHERE member_id = ? AND member_pw = ?";
+		
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, member.getMemberName());
+		stmt.setString(2, member.getMemberId());
+		stmt.setString(3, member.getMemberPw());
+		
+		resultLow = stmt.executeUpdate();
+		return resultLow; // 0 반환 -> 회원수정 실패, 1반환 -> 회원수정 성공
+	}
 	
 	// 4) 회원 삭제 dao
+	public int deleteMemberDao(Connection conn, Member member) throws Exception {
+		int resultLow = 0;
+		
+		String sql = "DELETE"
+				+ " FROM member"
+				+ " WHERE member_id = ? AND member_pw = ?";
+		
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, member.getMemberId());
+		stmt.setString(2, member.getMemberPw());
+		
+		resultLow = stmt.executeUpdate();
+		return resultLow; // 0 반환 -> 회원삭제 실패, 1반환 -> 회원삭제 성공
+	}
 }
